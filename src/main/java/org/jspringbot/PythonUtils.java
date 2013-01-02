741c78f05df504fa49fd6e5a810ba969c3543a12
Alvin R. de Leon <alvinrdeleon@me.com>
2013-01-02 10:59:37 +0800

    add phyton utility class

--- src/main/java/org/jspringbot/PythonUtils.java
+++ src/main/java/org/jspringbot/PythonUtils.java
@@ -4,7 +4,11 @@ import org.python.core.*;
 
 public class PythonUtils {
     public static Object toJava(Object arg) {
-        if(PyInteger.class.isInstance(arg)) {
+        if(PyUnicode.class.isInstance(arg)) {
+            return ((PyUnicode) arg).getString();
+        } else if(PyBoolean.class.isInstance(arg)) {
+            return ((PyBoolean) arg).getBooleanValue();
+        } else if(PyInteger.class.isInstance(arg)) {
             return ((PyInteger) arg).getValue();
         } else if(PyFloat.class.isInstance(arg)) {
             return ((PyFloat) arg).getValue();
