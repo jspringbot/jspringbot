2bc7f6f43f49c983650806bbef21d74d203e7028
alvinrdeleon <alvinrdeleon@me.com>
2015-05-08 06:43:34 +0800

    documentation updates.

--- assets/js/application.js
+++ assets/js/application.js
@@ -9,9 +9,9 @@
     var $window = $(window)
 
     // Disable certain links in docs
-    $('section [href^=#]').click(function (e) {
-      e.preventDefault()
-    })
+    //$('section [href^=#]').click(function (e) {
+    //  e.preventDefault()
+    //})
 
     // side bar
     $('.bs-docs-sidenav').affix({
