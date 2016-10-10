748daaa7f2e4507bc874991795a0a3dcc978fdf0
adeleon <alvinrdeleon@me.com>
2016-10-10 13:38:54 +0800

    added release notes.

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,5 +1,5 @@
 (function() {
   $.getJSON('metadata/app-metadata.json', function(data) {
-    $(".navbar-fixed-top .container p.navbar-text").html('<span class="hidden-phone">Version</span> ' + data.version);
+    $(".mainmenu .container p.navbar-text").html('<span class="hidden-phone">Version</span> ' + data.version);
   });
 })();
